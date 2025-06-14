ccb7838505bcfcb067662fd28fed9d5ccf9aaa26
Ongeri123 <ongerinewton10@gmail.com>
2025-06-15 02:03:54 +0300

    arithmetic operators

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let num1 = 2;
+let num2 = 31
+let multiply = (num1 * num2)
+console.log(multiply)
 
-// Write your code here
+let random = Math.floor(Math.random() * 10) + 1;
+console.log (random);
+ 
+let num3 = 11;
+let num4 = 7;
+let mod = (num3 % num4);
+console.log(mod);
+
+let max = Math.max(1,2,3,4,5,8,15,17,20);
+console.log(max);
